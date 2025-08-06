fba34fd7e68ed68b00866300e2f495a3ee2d8d94
Alina-skypro2025 <alin.guschina2018@yandex.ru>
2025-08-06 20:23:52 +0500

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const personalKey = "prod";
 const baseHost = "https://webdev-hw-api.vercel.app";
-const postsHost = ${baseHost}/api/v1/${personalKey}/instapro;
+const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
 export function getPosts({ token }) {
   return fetch(postsHost, {
