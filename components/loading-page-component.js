a7a53564be7f3224cc75558ead9db9bdebaf15af
Alina-skypro2025 <alin.guschina2018@yandex.ru>
2025-08-20 21:20:28 +0500

    Update loading-page-component.js

--- components/loading-page-component.js
+++ components/loading-page-component.js
@@ -1,26 +1,7 @@
-import { renderHeaderComponent } from "./header-component.js";
-
-
-export function renderLoadingPageComponent({ appEl, user, goToPage }) {
- 
-  const appHtml = `
-              <div class="page-container">
-                <div class="header-container"></div>
-                <div class="loading-page">
-                  <div class="loading-spinner">
-                    <div class="spinner"></div>
-                    <p>Загружаем данные...</p>
-                  </div>
-                </div>
-              </div>`;
-
- 
-  appEl.innerHTML = appHtml;
-
-
-  renderHeaderComponent({
-    user,
-    element: document.querySelector(".header-container"),
-    goToPage,
-  });
+export function renderLoadingPageComponent({ appEl }) {
+  const html = `
+    <div class="page-container">
+      <div class="loader"></div>
+    </div>`;
+  appEl.innerHTML = html;
 }
